77ae0995f369a4e5935da8089e555a7583e28ea3
Katherine Alvarez <kalvarez@nisum.com>
2018-11-10 08:06:49 -0300

    Primer commit

---
+++ GestionMascotas/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Mascota(models.Model):
+    codigo = models.CharField(max_length=10)
+    nombre =  models.CharField(max_length=30)
+    talla = (('XS', 'Extra pequeño'), ('S', 'Pequeño'), ('M', 'Mediano'), ('L', 'Grande'), ('XL', 'Extra grande'))
+    tamano = models.CharField(max_length=2, choices=talla, default='M')
+    peso = models.PositiveIntegerField()
+    color = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=50)
+    fechaRescate = models.DateField()
+    estados = (('Disponible', 'Disponible'), ('Rescatado', 'Rescatado'), ('Adoptado', 'Adoptado'))
+    estado = models.CharField(max_length=10, choices=estados, default='Rescatado')
